af9a9b85a857a6f83729429052dc285b18dd3df3
Xuan Yang <xuan.yang@goldenf.co>
2021-02-08 15:47:14 +0800

    init

---
+++ src/Contract/PgFishServiceInterface.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+namespace GiocoPlus\PgFish\Contract;
+/**
+ * PG漁機
+ */
+interface PgFishServiceInterface {
+
+    /**
+     * 遊戲啟動
+     *
+     * @param string $op_code
+     * @param string $account
+     * @param string $game_code
+     * @param string $language
+     * @return mixed
+     */
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language);
+    
+    /**
+     * 遊戲詳情
+     * @param string $op_code
+     * @param string $account
+     * @param string $bet_id
+     * @return mixed
+     */
+    function gameDetail(string $op_code, string $account, string $bet_id);
+
+}
+
